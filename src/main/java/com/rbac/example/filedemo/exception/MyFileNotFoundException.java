77406fdf05b85d3ab5dcfab5e541c28b3da9378a
wlfei <basicworld@163.com>
2022-02-10 00:09:47 +0800

    演示文件上传功能

---
+++ src/main/java/com/rbac/example/filedemo/exception/MyFileNotFoundException.java
@@ -0,0 +1,25 @@
+/**
+ *
+ */
+package com.rbac.example.filedemo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ *
+ * @author wlfei
+ * @date 2022-02-09
+ */
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class MyFileNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public MyFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public MyFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/main/java/com/rbac/example/filedemo/property/FileStorageProperties.java
@@ -0,0 +1,26 @@
+/**
+ *
+ */
+package com.rbac.example.filedemo.property;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author wlfei
+ * @date 2022-02-09
+ */
+@Component
+@ConfigurationProperties(prefix = "file")
+public class FileStorageProperties {
+    private String uploadDir;
+
+    public String getUploadDir() {
+        return uploadDir;
+    }
+
+    public void setUploadDir(String uploadDir) {
+        this.uploadDir = uploadDir;
+    }
+}
