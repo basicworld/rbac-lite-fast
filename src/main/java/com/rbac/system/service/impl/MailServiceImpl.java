bd32e8f385e192c5217c0a76b0c5c96df060c8bc
wlifei <wlifei@nifa.org.cn>
2022-02-25 23:46:41 +0800

    精简了邮件配置功能

--- src/main/java/com/rbac/system/service/impl/MailServiceImpl.java
+++ src/main/java/com/rbac/system/service/impl/MailServiceImpl.java
@@ -24,47 +24,47 @@ import com.rbac.system.service.ISysConfigService;
  */
 @Service
 public class MailServiceImpl implements IMailService {
-	private static final Logger logger = LoggerFactory.getLogger(MailServiceImpl.class);
+    private static final Logger logger = LoggerFactory.getLogger(MailServiceImpl.class);
 
-	/**
-	 * Spring Boot 提供了一个发送邮件的简单抽象，使用的是下面这个接口，这里直接注入即可使用
-	 */
-	@Autowired
-	private JavaMailSenderImpl mailSender;
+    /**
+     * Spring Boot 提供了一个发送邮件的简单抽象，使用的是下面这个接口，这里直接注入即可使用
+     */
+    @Autowired
+    private JavaMailSenderImpl mailSender;
 
-	@Autowired
-	private ISysConfigService configService;
+    @Autowired
+    private ISysConfigService configService;
 
-	@Value("${spring.mail.username}")
-	private String from;
+    @Value("${spring.mail.username}")
+    private String from;
 
-	@Override
-	public Boolean canSendMail() {
-		byte smtpEnable = configService.valueOfConfig(ConfigConstants.KEY_MAIL_SMTP_OPEN, BaseConstants.NO);
-		return (BaseConstants.YES == smtpEnable);
-	}
+    @Override
+    public Boolean canSendMail() {
+        byte smtpEnable = configService.valueOfConfig(ConfigConstants.KEY_MAIL_SMTP_OPEN, BaseConstants.NO);
+        return (BaseConstants.YES == smtpEnable);
+    }
 
-	@Override
-	public Boolean sendSimpleMail(String to, String subject, String content) {
-		if (!canSendMail()) {
-			logger.warn("邮箱功能未启用，不能发送邮件 {} 给 {}", subject, to);
-			return false;
-		}
+    @Override
+    public Boolean sendSimpleMail(String to, String subject, String content) {
+        if (!canSendMail()) {
+            logger.warn("邮箱功能未启用，不能发送邮件 {} 给 {}", subject, to);
+            return false;
+        }
 
-		// 创建SimpleMailMessage对象
-		SimpleMailMessage message = new SimpleMailMessage();
+        // 创建SimpleMailMessage对象
+        SimpleMailMessage message = new SimpleMailMessage();
 
-		message.setFrom(from);
-		// 邮件接收人
-		message.setTo(to);
-		// 邮件主题
-		message.setSubject(subject);
-		// 邮件内容
-		message.setText(content);
+        message.setFrom(from);
+        // 邮件接收人
+        message.setTo(to);
+        // 邮件主题
+        message.setSubject(subject);
+        // 邮件内容
+        message.setText(content);
 
-		mailSender.send(message);
+        mailSender.send(message);
 
-		return true;
-	}
+        return true;
+    }
 
 }
